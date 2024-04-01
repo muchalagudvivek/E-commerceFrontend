43257aeef588535f5c91810549b967aacb752648
Vivek Muchalagudd <muchalagudvivek484@gmail.com>
2024-04-01 20:37:13 +0530

    tailwind package

--- E-commerce Frontend/src/Context/ShopContext.jsx
+++ E-commerce Frontend/src/Context/ShopContext.jsx
@@ -43,7 +43,7 @@ const ShopContextProvider = (props) => {
     for (const item in cartItems) {
       if (cartItems[item] > 0) {
         let itemInfo = products.find((product) => product.id === Number(item));
-        totalAmount += cartItems[item] * itemInfo.new_price;
+        totalAmount += cartItems[item] * itemInfo?.new_price;
       }
     }
     return totalAmount;
